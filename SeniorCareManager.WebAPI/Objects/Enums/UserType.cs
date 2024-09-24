e00d89dd959654177420ec2d3efba1c7230ac638
DanielOliveira17 <danieloliveirask891@gmail.com>
2024-09-24 13:42:27 -0300

    :books: criação do enum UserType

---
+++ SeniorCareManager.WebAPI/Objects/Enums/UserType.cs
@@ -0,0 +1,15 @@
+﻿namespace SeniorCareManager.WebAPI.Objects.Enums
+{
+    public enum UserType
+    {
+        Admin = 1,
+        Manager = 2,
+        Operator =3,
+        Nurse = 4,
+        Physiotherapist = 5,
+        Nutritionist = 6,
+        Phychologist = 7,
+        Doctor = 8,
+        Nursingtechnician = 9,
+    }
+}
