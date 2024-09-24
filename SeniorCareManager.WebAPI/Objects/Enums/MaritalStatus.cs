bc40a24b78b044f892843c1884b851926aa14ba3
Felipe V. Souza <felipevillela.2006@gmail.com>
2024-09-24 13:52:25 -0300

    :books: docs desenvolvi MaritalStatus

---
+++ SeniorCareManager.WebAPI/Objects/Enums/MaritalStatus.cs
@@ -0,0 +1,13 @@
+﻿namespace SeniorCareManager.WebAPI.Objects.Enums
+{
+    public enum MaritalStatus
+    {
+        Single = 1,
+        Married = 2,
+        Widowed = 3,
+        Divorced = 4,
+        Separated = 5,
+        Civil_union = 6,
+
+    }
+}
