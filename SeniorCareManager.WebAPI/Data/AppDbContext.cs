c74f8a7790af19772a23d38f056b863d89d374f8
Jefferson Passerini <46405189+jeffersonarpasserini@users.noreply.github.com>
2024-09-30 15:05:10 -0300

    [20240930.01] Sprint 01 - Pull Requests (#50)
    * init: Configuração inicial do ambiente de desenvolvimento
    
    * feat: gitignore add in project
    
    * feat: EndPoint ProductGroup implemented
    
    * bug: correção gitignore
    
    * bug: correção gitignore
    
    * bug: correção gitignore
    
    * bug: correção gitignore
    
    * bug: correção gitignore
    
    * bug: correção gitignore
    
    * :sparkles: feat: Enumeration StockRequestType Desenvolvido
    
    * :sparkles: feat: Enum Ethnicity desenvolvido
    
    * :sparkles: feat: Enum EducationLevel desenvolvido
    
    * :sparkless: Criando o ENUM-SEX
    
    * :books: criação do enum UserType
    
    * :books: docs: Documentação do Enum StockRequestType
    
    * Desenvolvido o Enum YesNo
    
    * :books: docs desenvolvi MaritalStatus
    
    * :books: docs: Documentação Task #13
    
    * :books: 7 #1 DevEnumPursache
    
    * :sparkles: feat: Enum HealthPlanType
    
    * :books: docs: Sprint 01 Task#1 - Documentação Enum HealthPlanType
    
    * :books: docs: Documentação Task #11
    
    * :sparkless: Enum StockMovimentType desenvolvido
    
    * :books: Docks
    
    * :docs: Adicionando a documentacao da task DesenvolverEnumSex
    
    * :books: docs: Documentacao do Enum YesNo.
    
    * Correção do nome do arquivo
    
    * Alterando nome do arquivo da documentacao
    
    * Corrigindo as letras do Enums
    
    * :books: criação do enum UserType
    
    * :books: criação do enum UserType
    
    * Correção do PullRequest
    
    * :books ciração enum UserStatus
    
    * Alterando nome da Documentação
    
    * :books: finalização e correção
    
    * :books: Correcao nomenclatura da documentacao sprint
    
    ---------
    
    Co-authored-by: jeffersonarpasserini <jeffeson.passerini@gmail.com>
    Co-authored-by: marcoantonio-dev <maarcoantoniomasson@gmail.com>
    Co-authored-by: Larksel <lemuelgames@outlook.com>
    Co-authored-by: VINIshow1907 <viniciuslucio792@gmail.com>
    Co-authored-by: unknown <gabrielcasrellari45@gmail.com>
    Co-authored-by: DanielOliveira17 <danieloliveirask891@gmail.com>
    Co-authored-by: KetlynT <tonholiketlyn@gmail.com>
    Co-authored-by: Felipe V. Souza <felipevillela.2006@gmail.com>
    Co-authored-by: FrancianeRamos <francianeramos6492gmail.com>
    Co-authored-by: rodrigo-lombezzi <rodrigoylombezzi@gmail.com>
    Co-authored-by: PedroBesteti07 <pedrobestetirotondo@gmail.com>
    Co-authored-by: GuilhermeTakemoto <guilhermetakemoto2@gmail.com>
    Co-authored-by: RenatoSantos25 <renatosantosfilho2005@gmail.com>
    Co-authored-by: PedroBesteti07 <pedrobestetirootndo@gmail.com>
    Co-authored-by: GabrielJorge7 <gabrieljorgemarta@gmail.com>
    Co-authored-by: igorjiboia <igoraveledo230@gmail.com>

---
+++ SeniorCareManager.WebAPI/Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using SeniorCareManager.WebAPI.Data.Builders;
+using SeniorCareManager.WebAPI.Objects.Models;
+
+namespace SeniorCareManager.WebAPI.Data;
+
+public class AppDbContext: DbContext
+{
+    //recebe a conexão do Startup
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){}
+    
+    public DbSet<ProductGroup> ProductGroups { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Chamando Builder para configurar as entidades
+        ProductGroupBuilder.Build(modelBuilder);
+
+    }
+}
