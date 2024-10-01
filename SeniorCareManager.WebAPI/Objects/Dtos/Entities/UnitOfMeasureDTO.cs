8a89131b27dc889a1529e9e47a30ac175858b327
GuilhermeTakemoto <guilhermetakemoto2@gmail.com>
2024-10-01 14:23:11 -0300

    :sparkless: Adicionando, model, dtos, mapping e service da UnitOfMeasure2

--- SeniorCareManager.WebAPI/Objects/Dtos/Entities/UnitOfMeasureDTO.cs
+++ SeniorCareManager.WebAPI/Objects/Dtos/Entities/UnitOfMeasureDTO.cs
@@ -2,5 +2,8 @@
 {
     public class UnitOfMeasureDTO
     {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string Abbreviation { get; set; }
     }
 }
--- SeniorCareManager.WebAPI/Objects/Models/UnitOfMeasure.cs
+++ SeniorCareManager.WebAPI/Objects/Models/UnitOfMeasure.cs
@@ -1,6 +1,32 @@
-﻿namespace SeniorCareManager.WebAPI.Objects.Models
+﻿
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SeniorCareManager.WebAPI.Objects.Models;
+[Table("unitofmeasure")]
+
+public class UnitOfMeasure
 {
-    public class UnitOfMeasure
+    [Column("id")]
+    public int Id { get; set; }
+
+
+    [Column("description")]
+    public string Description { get; set; }
+
+
+    [Column("abbreviation")]
+    public string Abbreviation { get; set; }
+
+
+    public UnitOfMeasure()
     {
+
+    }
+
+    public UnitOfMeasure(int id, string abbreviation, string description)
+    {
+        Id = id;
+        Description = description;
+        Abbreviation = abbreviation;
     }
 }
