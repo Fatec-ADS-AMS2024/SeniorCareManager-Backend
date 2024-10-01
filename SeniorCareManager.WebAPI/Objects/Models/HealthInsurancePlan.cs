88fc791bf064b29c65a69e176136aa13a8fcb212
Larksel <lemuelgames@outlook.com>
2024-10-01 13:36:36 -0300

    :sparkles: feat: Model HealthInsurancePlan desenvolvida

---
+++ SeniorCareManager.WebAPI/Objects/Models/HealthInsurancePlan.cs
@@ -0,0 +1,21 @@
+﻿using SeniorCareManager.WebAPI.Objects.Enums;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SeniorCareManager.WebAPI.Objects.Models;
+
+[Table("healthinsuranceplan")]
+public class HealthInsurancePlan
+{
+    [Column("id")]
+    public int Id { get; set; }
+
+    [Column("name")]
+    public string Name { get; set; }
+
+    [Column("type")]
+    public HealthPlanType Type { get; set; }
+
+    [Column("abbreviation")]
+    public string Abbreviation { get; set; }
+}
+
