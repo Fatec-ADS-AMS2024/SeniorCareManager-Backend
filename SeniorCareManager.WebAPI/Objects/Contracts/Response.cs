e90a517ee122f1b282c943277d9224f0a92e2ce9
rodrigo-lombezzi <rodrigoylombezzi@gmail.com>
2025-06-03 10:48:09 -0300

    :sparkles: feat: Validação de strings

---
+++ SeniorCareManager.WebAPI/Objects/Contracts/Response.cs
@@ -0,0 +1,11 @@
+﻿using SeniorCareManager.WebAPI.Objects.Enums;
+
+namespace SeniorCareManager.WebAPI.Services.Utils
+{
+    public class Response
+    {
+        public ResponseEnum Code { get; set; }
+        public string? Message { get; set; }
+        public object? Data { get; set; }
+    }
+}
