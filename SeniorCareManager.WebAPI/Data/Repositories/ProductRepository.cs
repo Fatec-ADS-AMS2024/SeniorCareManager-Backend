1072fb7e78cdbb8e8898f1390a8eec4598dd40c3
RenatoSantos25 <renatosantosfilho2005@gmail.com>
2025-06-02 23:51:38 -0300

    :Sparkles: Adicionando Builder da Product e corrigindo a Repository

--- SeniorCareManager.WebAPI/Data/Repositories/ProductRepository.cs
+++ SeniorCareManager.WebAPI/Data/Repositories/ProductRepository.cs
@@ -1,39 +1,15 @@
 using SeniorCareManager.WebAPI.Data.Interfaces;
 using SeniorCareManager.WebAPI.Objects.Models;
 
-namespace SeniorCareManager.WebAPI.Data.Repositories;
-
-public class ProductRepository : GenericRepository<ProductGroup>, IProductRepository
+namespace SeniorCareManager.WebAPI.Data.Repositories
 {
-    private readonly AppDbContext _context;
+	public class ProductRepository : GenericRepository<Product>, IProductRepository
+	{
+		private readonly AppDbContext _context;
 
-    public ProductRepository(AppDbContext context) : base(context)
-    {
-        this._context = context;
-    }
-
-    public Task Add(Product entity)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task Remove(Product entity)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task Update(Product entity)
-    {
-        throw new NotImplementedException();
-    }
-
-    Task<IEnumerable<Product>> IGenericRepository<Product>.Get()
-    {
-        throw new NotImplementedException();
-    }
-
-    Task<Product> IGenericRepository<Product>.GetById(int id)
-    {
-        throw new NotImplementedException();
-    }
+		public ProductRepository(AppDbContext context) : base(context)
+		{
+			this._context = context;
+		}
+	}
 }
