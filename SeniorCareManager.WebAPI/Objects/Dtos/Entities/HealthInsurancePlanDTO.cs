cb1d8296a380bf729fe80c4202788a33d21bd139
rodrigo-lombezzi <rodrigoylombezzi@gmail.com>
2025-06-12 07:39:51 -0300

    :bug: fix: Validações de formato na dto

--- SeniorCareManager.WebAPI/Objects/Dtos/Entities/HealthInsurancePlanDTO.cs
+++ SeniorCareManager.WebAPI/Objects/Dtos/Entities/HealthInsurancePlanDTO.cs
@@ -4,9 +4,19 @@ namespace SeniorCareManager.WebAPI.Objects.Dtos.Entities;
 public class HealthInsurancePlanDTO
 {
     public int Id { get; set; }
-    public string Name { get; set; }
     public int Type { get; set; }
-    public string Abbreviation { get; set; }
+    private string _name;
+    public string Name
+    {
+        get => _name;
+        set => _name = value.Trim();
+    }
+    private string _abbreviation;
+    public string Abbreviation
+    {
+        get => _abbreviation;
+        set => _abbreviation = value.Trim();
+    }
     public static bool IsFilledString(params string[] parametros)
     {
         foreach (var parametro in parametros)
@@ -18,7 +28,6 @@ public class HealthInsurancePlanDTO
         }
         return true;
     }
-
     public bool EnumValid()
     {
         if (!Enum.IsDefined(typeof(HealthPlanType), Type))
