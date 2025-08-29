da0e7285a340eafa9151b54246a8678806eda9ff
rodrigo-lombezzi <rodrigoylombezzi@gmail.com>
2025-08-29 16:27:19 -0300

    :sparkles: feat: Validação cpf

---
+++ SeniorCareManager.WebAPI/Objects/Dtos/DataAnnotations/Format/ExtractNumbers.cs
@@ -0,0 +1,28 @@
+﻿using SeniorCareManager.WebAPI.Objects.Dtos.DataAnnotations.Base;
+using SeniorCareManager.WebAPI.Services.Utils;
+
+namespace SeniorCareManager.WebAPI.Objects.Dtos.DataAnnotations.Format;
+public class ExtractNumbers : BaseAnnotation
+{
+    public ExtractNumbers(params object[]? parameters) : base(parameters)
+    {
+        if (parameters is null)
+            throw new ArgumentNullException("Essa funcão precisa de parâmetros");
+    }
+    public static string ExtractNum(this string text)
+    {
+        if (string.IsNullOrEmpty(text))
+            return string.Empty;
+
+        return new string(text.Where(char.IsDigit).ToArray());
+    }
+
+    public override void Execute()
+    {
+        string valor = Value?.ToString();
+
+        if (string.IsNullOrWhiteSpace(valor))
+            ReturnError("O campo não pode ser nulo ou vazio.");
+        valor = valor.ExtractNumbers();
+    }
+}
--- SeniorCareManager.WebAPI/Objects/Dtos/Entities/ManufacturerDTO.cs
+++ SeniorCareManager.WebAPI/Objects/Dtos/Entities/ManufacturerDTO.cs
@@ -5,6 +5,7 @@ public class ManufacturerDTO
     public int Id { get; set; }
     public string CorporateName { get; set; }
     public string TradeName { get; set; }
+    [CpfValidator]
     public string CpfCnpj { get; set; }
     [PhoneValidator]
     public string Phone { get; set; }
