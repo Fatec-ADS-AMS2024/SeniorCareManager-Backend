cb70eb9f5a43c106dc8e3e73164be5bd187d059a
rodrigo-lombezzi <rodrigoylombezzi@gmail.com>
2025-09-04 14:03:10 -0300

    :bug: fix: HealthInsurancePlanService validação de nome

--- SeniorCareManager.WebAPI/Objects/Dtos/Entities/HealthInsurancePlanDTO.cs
+++ SeniorCareManager.WebAPI/Objects/Dtos/Entities/HealthInsurancePlanDTO.cs
@@ -1,37 +1,19 @@
-﻿using SeniorCareManager.WebAPI.Objects.Enums;
+﻿using SeniorCareManager.WebAPI.Objects.Dtos.DataAnnotations.Format;
+using SeniorCareManager.WebAPI.Objects.Dtos.DataAnnotations.Valid;
 
 namespace SeniorCareManager.WebAPI.Objects.Dtos.Entities;
 public class HealthInsurancePlanDTO
 {
     public int Id { get; set; }
+    [NullOrEmpty(ErrorMessage = "O campo tipo não pode ser nulo ou vazio.")]
     public int Type { get; set; }
-    private string _name;
-    public string Name
-    {
-        get => _name;
-        set => _name = value.Trim();
-    }
-    private string _abbreviation;
-    public string Abbreviation
-    {
-        get => _abbreviation;
-        set => _abbreviation = value.Trim();
-    }
-    public static bool IsFilledString(params string[] parametros)
-    {
-        foreach (var parametro in parametros)
-        {
-            if (string.IsNullOrWhiteSpace(parametro))
-            {
-                return false;
-            }
-        }
-        return true;
-    }
-    public bool EnumValid()
-    {
-        if (!Enum.IsDefined(typeof(HealthPlanType), Type))
-            return false;
-        return true; 
-    }
+    [NullOrEmpty(ErrorMessage = "O campo tipo não pode ser nulo ou vazio.")]
+    [RemoveSpaces]
+
+    public string Name { get; set; }
+    [NullOrEmpty(ErrorMessage = "O campo nome tipo não pode ser nulo ou vazio.")]
+    [RemoveSpaces]
+    [UpperCaracters]
+    public string Abbreviation { get; set; }
+
 }
