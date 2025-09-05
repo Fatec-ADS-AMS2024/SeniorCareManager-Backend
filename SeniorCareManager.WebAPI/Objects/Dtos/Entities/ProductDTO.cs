a8377438a05de6e26c45c6be24a63dab4a6518eb
Larksel <lemuelgames@outlook.com>
2025-09-05 17:30:56 -0300

    :sparkles:Feat: Adicionando Data Anotations como validações na DTO

--- SeniorCareManager.WebAPI/Objects/Dtos/Entities/ProductDTO.cs
+++ SeniorCareManager.WebAPI/Objects/Dtos/Entities/ProductDTO.cs
@@ -1,3 +1,5 @@
+using SeniorCareManager.WebAPI.Objects.Dtos.DataAnnotations.Format;
+using SeniorCareManager.WebAPI.Objects.Dtos.DataAnnotations.Valid;
 using SeniorCareManager.WebAPI.Objects.Enums;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Xml.Linq;
@@ -9,21 +11,37 @@ namespace SeniorCareManager.WebAPI.Objects.Dtos.Entities
     {
         public long Id { get; set; }
 
+        [NullOrEmpty(ErrorMessage = "Descrição obrigatória.")]
+        [RemoveSpaces]
         public string Description { get; set; }
 
+        [NullOrEmpty(ErrorMessage = "Nome genérico obrigatório.")]
+        [RemoveSpaces]
         public string GenericName { get; set; }
 
+        [NullOrEmpty(ErrorMessage = "Estoque minímo obrigatório.")]
+        [RemoveSpaces]
+        [NumValidator(ErrorMessage = "Estoque minimo tem que ser maior que zero")]
         public decimal MinimumStock { get; set; }
 
+        [NullOrEmpty(ErrorMessage = "Quantidade atual do estoque obrigatório.")]
+        [RemoveSpaces]
         public decimal CurrentStock { get; set; }
 
+        [NullOrEmpty(ErrorMessage = "Valor do estoque obrigatório.")]
+        [RemoveSpaces]
         public decimal StockValue { get; set; }
 
+        [NullOrEmpty(ErrorMessage = "Preço unitário obrigatório.")]
+        [RemoveSpaces]
         public decimal UnitPrice { get; set; }
 
         public decimal AverageCost { get; set; }
 
+        [NullOrEmpty(ErrorMessage = "Preço da última compra obrigatório.")]
+        [RemoveSpaces]
         public decimal LastPurchasePrice { get; set; }
+
 
         public YesNo HighCost { get; set; }
 
