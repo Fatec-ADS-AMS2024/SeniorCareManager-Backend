8c8791c3a1147f52cdc9a6634dfabd6303144012
RenatoSantos25 <renatosantosfilho2005@gmail.com>
2025-09-24 15:53:05 -0300

    Adicionando novo padrão na controller

--- SeniorCareManager.WebAPI/Controllers/EmployeeController.cs
+++ SeniorCareManager.WebAPI/Controllers/EmployeeController.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
+using SeniorCareManager.WebAPI.Objects.Contracts;
+using SeniorCareManager.WebAPI.Objects.Dtos.DataAnnotations.Base;
 using SeniorCareManager.WebAPI.Objects.Dtos.Entities;
 using SeniorCareManager.WebAPI.Objects.Models;
+using SeniorCareManager.WebAPI.Services.Entities;
 using SeniorCareManager.WebAPI.Services.Interfaces;
 
 namespace SeniorCareManager.WebAPI.Controllers
@@ -20,30 +23,27 @@ namespace SeniorCareManager.WebAPI.Controllers
         public async Task<IActionResult> GetAll()
         {
             var employees = await _employeeService.GetAll();
-            return Ok(employees);
+            return Response<IEnumerable<EmployeeDTO>>.Ok(employees, "Lista de funcionarios obtida com sucesso!");
         }
 
         [HttpGet("{id}")]
         public async Task<IActionResult> GetById(int id)
         {
             var employee = await _employeeService.GetById(id);
-            if (employee == null)
-                return NotFound("Funcionário não encontrado");
-            return Ok(employee);
+            return Response<EmployeeDTO>.Ok(employee, "Funcionário obtido com sucesso!");
         }
 
         [HttpPost]
-        public async Task<IActionResult> Post(EmployeeDTO employee)
+        public async Task<IActionResult> Post(EmployeeDTO employeeDto)
         {
-            try
-            {
-                await _employeeService.Create(employee);
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, "Ocorreu um erro ao tentar inserir um novo funcionário");
-            }
-            return Ok(employee);
+           
+            Execute.Executar(employeeDto);
+            employeeDto.Id = 0;
+            await _employeeService.Create(employeeDto);
+
+            return Response<EmployeeDTO>.Created(employeeDto, "Cargo Cadastrado com sucesso!");
+
+            return Ok(employeeDto);
         }
 
         [HttpPut("{id}")]
