d82ac1006110f81f113f00fd2c7a4e7b4a50d33f
RenatoSantos25 <renatosantosfilho2005@gmail.com>
2025-10-28 21:27:37 -0300

    :sparkles: Adicionando padrao da controller e validação na DTO

--- SeniorCareManager.WebAPI/Objects/Dtos/Entities/UnitOfMeasureDTO.cs
+++ SeniorCareManager.WebAPI/Objects/Dtos/Entities/UnitOfMeasureDTO.cs
@@ -1,9 +1,17 @@
-﻿namespace SeniorCareManager.WebAPI.Objects.Dtos.Entities
+﻿using SeniorCareManager.WebAPI.Objects.Dtos.DataAnnotations.Format;
+using SeniorCareManager.WebAPI.Objects.Dtos.DataAnnotations.Valid;
+
+namespace SeniorCareManager.WebAPI.Objects.Dtos.Entities
 {
     public class UnitOfMeasureDTO
     {
         public int Id { get; set; }
+
         public string Description { get; set; }
+
+        [NullOrEmpty(ErrorMessage = "O campo não pode ser nulo ou vazio.")]
+        [RemoveSpaces]
+        [QtdCaractersValidator(3!, "No máximo 3 caracteres!")]
         public string Abbreviation { get; set; }
     }
 }
