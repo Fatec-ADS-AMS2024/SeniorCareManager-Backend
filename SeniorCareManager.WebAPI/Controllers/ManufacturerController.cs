5fc30e2b27cae62aff7c6956034ea1c26c8d795b
FrancianeRamos <francianeramos649@gmail.com>
2025-11-07 15:32:14 -0300

    :sparkles: Implementação de validações e padronizações

--- SeniorCareManager.WebAPI/Controllers/ManufacturerController.cs
+++ SeniorCareManager.WebAPI/Controllers/ManufacturerController.cs
@@ -1,82 +1,60 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SeniorCareManager.WebAPI.Objects.Contracts;
 using SeniorCareManager.WebAPI.Objects.Dtos.DataAnnotations.Base;
-using SeniorCareManager.WebAPI.Objects.Dtos.Entities;
-using SeniorCareManager.WebAPI.Objects.Models;
 using SeniorCareManager.WebAPI.Services.Interfaces;
 
-namespace SeniorCareManager.WebAPI.Controllers
+namespace SeniorCareManager.WebAPI.Controllers;
+
+[ApiController]
+[Route("api/v{version:apiVersion}/[controller]")]
+[ApiVersion("1")]
+[Authorize]
+public class ManufacturerController : Controller
 {
-    [ApiController]
-    [Route("api/v1/[controller]")]
-    public class ManufacturerController : ControllerBase
+    private readonly IManufacturerService _manufacturerService;
+
+    public ManufacturerController(IManufacturerService service)
     {
-        private readonly IManufacturerService _manufacturerService;
+        _manufacturerService = service;
+    }
 
-        public ManufacturerController(IManufacturerService service)
-        {
-            _manufacturerService = service;
-        }
+    [HttpGet, MapToApiVersion("1")]
+    public async Task<IActionResult> Get()
+    {
+        var manufacturers = await _manufacturerService.GetAll();
+        return Response<IEnumerable<ManufacturerDTO>>.Ok(manufacturers, "Lista de fabricantes obtida com sucesso!");
+    }
 
-        [HttpGet]
-        public async Task<IActionResult> Get()
-        {
-            var manufacturers = await _manufacturerService.GetAll();
-            return Ok(manufacturers);
-        }
+    [HttpGet("{id}"), MapToApiVersion("1")]
+    public async Task<IActionResult> GetById(int id)
+    {
+        var manufacturer = await _manufacturerService.GetById(id);
+        return Response<ManufacturerDTO>.Ok(manufacturer, "Fabricante obtido com sucesso!");
+    }
 
-        [HttpGet("{id}")]
-        public async Task<IActionResult> GetById(int id)
-        {
-            var manufacturer = await _manufacturerService.GetById(id);
-            if (manufacturer == null) return NotFound("Fabricante não encontrado!");
-            return Ok(manufacturer);
-        }
+    [HttpPost, MapToApiVersion("1")]
+    public async Task<IActionResult> Post(ManufacturerDTO manufacturerDto)
+    {
+        Execute.Executar(manufacturerDto);
+        manufacturerDto.Id = 0;
 
-        [HttpPost]
-        public async Task<IActionResult> Post(ManufacturerDTO manufacturer)
-        {
-            try
-            {
-                Execute.Executar(manufacturer);
-                await _manufacturerService.Create(manufacturer);
-            }
-            catch (Exception)
-            {
-                return StatusCode(500, "Ocorreu um erro ao tentar inserir um novo fabricante.");
-            }
-            return CreatedAtAction(nameof(GetById), new { id = manufacturer.Id }, manufacturer);
-        }
+        return Response<ManufacturerDTO>.Created(await _manufacturerService.Create(manufacturerDto), "Fabricante cadastrado com sucesso!");
+    }
 
-        [HttpPut("{id}")]
-        public async Task<IActionResult> Put(int id, ManufacturerDTO manufacturer)
-        {
-            try
-            {
-                await _manufacturerService.Update(manufacturer, id);
-            }
-            catch (Exception)
-            {
-                return StatusCode(500, "Ocorreu um erro ao tentar atualizar o fabricante.");
-            }
-            return Ok(manufacturer);
-        }
+    [HttpPut("{id}"), MapToApiVersion("1")]
+    public async Task<IActionResult> Put(int id, ManufacturerDTO manufacturerDto)
+    {
+        Execute.Executar(manufacturerDto);
+        await _manufacturerService.Update(manufacturerDto, id);
 
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> Delete(int id)
-        {
-            try
-            {
-                await _manufacturerService.Remove(id);
-                return Ok("Fabricante apagado com sucesso");
-            }
-            catch (KeyNotFoundException ex)
-            {
-                return NotFound(ex.Message);
-            }
-            catch (Exception)
-            {
-                return StatusCode(500, "Ocorreu um erro ao tentar remover o fabricante.");
-            }
-        }
+        return Response<ManufacturerDTO>.Ok(manufacturerDto, "Fabricante atualizado com sucesso!");
+    }
+
+    [HttpDelete("{id}"), MapToApiVersion("1")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        await _manufacturerService.Remove(id);
+        return Response<object>.NoContent();
     }
 }
