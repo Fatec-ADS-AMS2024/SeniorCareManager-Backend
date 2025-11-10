c6a0c18de45282a4eeb65ada86509caea0a87701
Gabriel Neto Siqueira <gabriel272173siqueira@gmail.com>
2025-11-10 19:19:35 -0300

    :recycle: refactor: Alterando método base de annotation.

--- SeniorCareManager.WebAPI/Objects/Dtos/DataAnnotations/Format/PhoneFormat.cs
+++ SeniorCareManager.WebAPI/Objects/Dtos/DataAnnotations/Format/PhoneFormat.cs
@@ -12,11 +12,13 @@ public class PhoneFormat : BaseAnnotation
 
         string valor = new string(Value.ToString()?.Where(char.IsDigit).ToArray());
 
+        Console.WriteLine($"Telefone: {valor}");
+
         if (valor.Length != 10 && valor.Length != 11)
             return ReturnError(NameProperty, "Telefone inválido.");
-        if (valor.Length == 11 && valor[2] != '9')
-            return ReturnError(NameProperty, "Número de celular inválido.");
+        Console.WriteLine($"Telefone sem formatação: {Value}");
         Value = valor;
+        Console.WriteLine($"Telefone formatado: {Value}");
         return null;
     }
 }
