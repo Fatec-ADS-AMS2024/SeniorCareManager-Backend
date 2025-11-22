1b58dd0c73a771324858b5c28565c0bda779bc5f
VINIshow1907 <viniciuslucio792@gmail.com>
2025-11-22 14:45:13 -0300

    :sparkles: feat: Arrumando validações na Supplier

--- SeniorCareManager.WebAPI/Controllers/SupplierController.cs
+++ SeniorCareManager.WebAPI/Controllers/SupplierController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using SeniorCareManager.WebAPI.Objects.Contracts;
 using SeniorCareManager.WebAPI.Objects.Dtos;
 using SeniorCareManager.WebAPI.Objects.Dtos.DataAnnotations.Base;
@@ -9,32 +10,37 @@ namespace SeniorCareManager.WebAPI.Controllers
     [ApiController]
     [Route("api/v{version:apiVersion}/[controller]")]
     [ApiVersion("1")]
+    [Authorize]
     public class SupplierController : ControllerBase
     {
         private readonly ISupplierService _supplierService;
 
         public SupplierController(ISupplierService service)
         {
-            this._supplierService = service;
+            _supplierService = service;
         }
 
         [HttpGet, MapToApiVersion("1")]
         public async Task<IActionResult> Get()
         {
-            return Response<object>.Ok(await _supplierService.GetAll(), "Fornecedores obtidos com sucesso!");
+            var suppliers = await _supplierService.GetAll();
+            return Response<object>.Ok(suppliers, "Fornecedores obtidos com sucesso!");
         }
 
         [HttpGet("{id}"), MapToApiVersion("1")]
         public async Task<IActionResult> GetById(int id)
         {
-            return Response<object>.Ok(await _supplierService.GetById(id), "Fornecedor obtido com sucesso!");
+            var supplier = await _supplierService.GetById(id);
+            return Response<object>.Ok(supplier, "Fornecedor obtido com sucesso!");
         }
 
-        [HttpPost]
-        public async Task<IActionResult> Post(SupplierDTO supplier)
+        [HttpPost, MapToApiVersion("1")]
+        public async Task<IActionResult> Post([FromBody] SupplierDTO supplier)
         {
             Execute.Executar(supplier);
-            return Response<object>.Created(await _supplierService.Create(supplier), "Fornecedor cadastrado com sucesso!");
+            supplier.Id = 0;
+            var created = await _supplierService.Create(supplier);
+            return Response<object>.Created(created, "Fornecedor cadastrado com sucesso!");
         }
 
         [HttpPut("{id}"), MapToApiVersion("1")]
@@ -53,3 +59,4 @@ namespace SeniorCareManager.WebAPI.Controllers
         }
     }
 }
+
--- SeniorCareManager.WebAPI/Services/Interfaces/ISupplierService.cs
+++ SeniorCareManager.WebAPI/Services/Interfaces/ISupplierService.cs
@@ -1,12 +1,9 @@
-﻿using SeniorCareManager.WebAPI.Objects.Dtos;
+using SeniorCareManager.WebAPI.Objects.Dtos;
 using SeniorCareManager.WebAPI.Objects.Models;
 
 namespace SeniorCareManager.WebAPI.Services.Interfaces;
 
 public interface ISupplierService : IGenericService<Supplier, SupplierDTO>
 {
-    public interface ISupplierService : IGenericService<Supplier, SupplierDTO>
-    {
-        Task<bool> CheckDuplicates(Func<Supplier, string?> selector, string? valor, int idIgnor);
-    }
+    Task<bool> CheckDuplicates(Func<Supplier, string?> selector, string? valor, int idIgnor);
 }
