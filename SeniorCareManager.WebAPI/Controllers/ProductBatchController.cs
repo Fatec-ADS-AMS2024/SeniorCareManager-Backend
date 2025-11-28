5473699a7d13a1f51d8091171b356d1be7ff2d3c
marcoantonio-dev <maarcoantoniomasson@gmail.com>
2025-11-28 16:31:30 -0300

    :sparkles: feat: standardizing and include validations

--- SeniorCareManager.WebAPI/Controllers/ProductBatchController.cs
+++ SeniorCareManager.WebAPI/Controllers/ProductBatchController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using SeniorCareManager.WebAPI.Objects.Contracts;
 using SeniorCareManager.WebAPI.Objects.Dtos.DataAnnotations.Base;
 using SeniorCareManager.WebAPI.Objects.Dtos.Entities;
@@ -7,7 +8,9 @@ using SeniorCareManager.WebAPI.Services.Interfaces;
 namespace SeniorCareManager.WebAPI.Controllers
 {
     [ApiController]
-    [Route("api/v1/[controller]")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiVersion("1")]
+    [Authorize]
     public class ProductBatchController : Controller
     {
         private readonly IProductBatchService _productBatchService;
@@ -17,59 +20,58 @@ namespace SeniorCareManager.WebAPI.Controllers
             this._productBatchService = service;
         }
 
-        [HttpGet]
+        [HttpGet, MapToApiVersion("1")]
         public async Task<IActionResult> Get()
         {
-            var list = await _productBatchService.GetAll();
-            return Response<IEnumerable<ProductBatchDTO>>.Ok(list, "Lista de lotes obtida com sucesso!");
+            var productBatches = await _productBatchService.GetAll();
+            return Response<IEnumerable<ProductBatchDTO>>.Ok(productBatches, "Lista de lotes obtida com sucesso!");
         }
 
-        [HttpGet("{id}")]
+        [HttpGet("{id}"), MapToApiVersion("1")]
         public async Task<IActionResult> GetById(long id)
         {
-            var dto = await _productBatchService.GetById(id);
-            return Response<ProductBatchDTO>.Ok(dto, "Lote obtido com sucesso!");
+            var productBatch = await _productBatchService.GetById(id);
+            return Response<ProductBatchDTO>.Ok(productBatch, "Lote obtido com sucesso!");
         }
 
-        [HttpGet("product/{productId}")]
+        [HttpGet("product/{productId}"), MapToApiVersion("1")]
         public async Task<IActionResult> GetByProduct(long productId)
         {
-            var list = await _productBatchService.GetByProduct(productId);
-            return Response<IEnumerable<ProductBatchDTO>>.Ok(list, "Lotes por produto obtidos com sucesso!");
+            var productBatches = await _productBatchService.GetByProduct(productId);
+            return Response<IEnumerable<ProductBatchDTO>>.Ok(productBatches, "Lotes por produto obtidos com sucesso!");
         }
 
-        [HttpGet("expiring")]
+        [HttpGet("expiring"), MapToApiVersion("1")]
         public async Task<IActionResult> GetExpiringBatches([FromQuery] int daysAhead = 30)
         {
             var list = await _productBatchService.GetExpiringBatches(daysAhead);
             return Response<IEnumerable<ProductBatchDTO>>.Ok(list, "Lotes próximos do vencimento obtidos com sucesso!");
         }
 
-        [HttpGet("expired")]
+        [HttpGet("expired"), MapToApiVersion("1")]
         public async Task<IActionResult> GetExpiredBatches()
         {
             var list = await _productBatchService.GetExpiredBatches();
             return Response<IEnumerable<ProductBatchDTO>>.Ok(list, "Lotes vencidos obtidos com sucesso!");
         }
 
-        [HttpPost]
-        public async Task<IActionResult> Post(ProductBatchDTO dto)
+        [HttpPost, MapToApiVersion("1")]
+        public async Task<IActionResult> Post(ProductBatchDTO productBatchDto)
         {
-            Execute.Executar(dto);
-            dto.Id = 0;
-            await _productBatchService.Create(dto);
-            return Response<ProductBatchDTO>.Created(dto, "Lote cadastrado com sucesso!");
+            Execute.Executar(productBatchDto);
+            productBatchDto.Id = 0;
+            return Response<ProductBatchDTO>.Created(await _productBatchService.Create(productBatchDto), "Lote cadastrado com sucesso!");
         }
 
-        [HttpPut("{id}")]
-        public async Task<IActionResult> Put(long id, ProductBatchDTO dto)
+        [HttpPut("{id}"), MapToApiVersion("1")]
+        public async Task<IActionResult> Put(long id, ProductBatchDTO productBatchDto)
         {
-            Execute.Executar(dto);
-            await _productBatchService.Update(dto, id);
-            return Response<ProductBatchDTO>.Ok(dto, "Lote atualizado com sucesso!");
+            Execute.Executar(productBatchDto);
+            await _productBatchService.Update(productBatchDto, id);
+            return Response<ProductBatchDTO>.Ok(productBatchDto, "Lote atualizado com sucesso!");
         }
 
-        [HttpDelete("{id}")]
+        [HttpDelete("{id}"), MapToApiVersion("1")]
         public async Task<IActionResult> Delete(long id)
         {
             await _productBatchService.Remove(id);
